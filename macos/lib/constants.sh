9992f64e9baedaddb8b1a794ebb3c65d5c2357ed
Hiroshi OTANI <hotani3@gmail.com>
2024-12-31 22:10:41 +0900

    Update default versions of JavaScript and Python

--- macos/lib/constants.sh
+++ macos/lib/constants.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
-readonly SCRIPT_VERSION="1.1.1"
+readonly SCRIPT_VERSION="1.1.2"
 readonly NVM_VERSION="0.40.1"
 
-readonly DEFAULT_PYTHON_VERSION="3.12.7"
-readonly DEFAULT_NODE_VERSION="22.11.0"
+readonly DEFAULT_PYTHON_VERSION="3.12.8"
+readonly DEFAULT_NODE_VERSION="22.12.0"
 
 readonly CPU_ARCH=$(uname -m)
 readonly MACOS_VERSION=$(sw_vers -productVersion)
