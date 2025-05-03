f1f6a3c34229b778cdb1ae49aae2f54c4f2e82e1
Hiroshi OTANI <hotani3@gmail.com>
2025-05-03 22:39:19 +0900

    Add tests on macOS Sequoia (arm64)

--- macos/lib/constants.sh
+++ macos/lib/constants.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 readonly SCRIPT_VERSION="1.1.2"
-readonly NVM_VERSION="0.40.1"
+readonly NVM_VERSION="0.40.2"
 
-readonly DEFAULT_NODE_VERSION="22.12.0"
+readonly DEFAULT_NODE_VERSION="22.15.0"
 readonly DEFAULT_PYTHON_VERSION="3.12.10"
 readonly DEFAULT_ANSIBLE_VERSION="2.17.11"
 
