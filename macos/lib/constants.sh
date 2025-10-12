69ce975b1f7911c408dbda0b2a5bd34b871555aa
Hiroshi OTANI <hotani3@gmail.com>
2025-10-12 21:54:07 +0900

    Drop support for macOS Ventura in Node.js

--- macos/lib/constants.sh
+++ macos/lib/constants.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 readonly SCRIPT_VERSION="1.2.0"
-readonly NVM_VERSION="0.40.2"
+readonly NVM_VERSION="0.40.3"
 
 readonly DEFAULT_NODE_VERSION="22.15.0"
 readonly DEFAULT_PYTHON_VERSION="3.12.10"
