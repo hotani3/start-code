82a3acd93264e4e329dd525ae41dcb523adbdfb4
Hiroshi OTANI <hotani3@gmail.com>
2025-12-07 17:08:47 +0900

    Define Install-Git function

--- windows/lib/Scoop.psm1
+++ windows/lib/Scoop.psm1
@@ -19,9 +19,23 @@ function Install-Scoop {
 
     $result = Detect -PackageTitle $packageTitle -DetectCommand $detectCommand -VersionCommand $versionCommand
     if ($result -ne 0) {
+        Switch-ExecutionPolicy
         Install -PackageTitle $packageTitle -InstallCommand $installCommand
         Detect -PackageTitle $packageTitle -DetectCommand $detectCommand -VersionCommand $versionCommand -ExitOnFail
     }
 }
 
-Export-ModuleMember -Function Install-Scoop
+function Install-Git {
+    $packageTitle = 'Git'
+    $detectCommand = 'git --version *>&1'
+    $versionCommand = "git --version *>&1 | Select-String -Pattern '(\d+\.\d+\.\d+\.windows\.\d+)' | ForEach-Object { `$_.Matches.Groups[1].Value } | Select-Object -First 1"
+    $installCommand = 'scoop install git'
+
+    $result = Detect -PackageTitle $packageTitle -DetectCommand $detectCommand -VersionCommand $versionCommand
+    if ($result -ne 0) {
+        Install -PackageTitle $packageTitle -InstallCommand $installCommand
+        Detect -PackageTitle $packageTitle -DetectCommand $detectCommand -VersionCommand $versionCommand -ExitOnFail
+    }
+}
+
+Export-ModuleMember -Function Install-Scoop, Install-Git
